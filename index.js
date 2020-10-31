ff92ffa6809aaa58540cabb976527a2a33cb97ca
Jorge Claro <jorgeclaroweb@gmail.com>
2020-10-31 21:59:19 +0000

    Changed response to json

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const fetch = require('node-fetch');
 const redis = require('redis');
 const cors = require('cors');
 
+
 const PORT = process.env.PORT || 5000;
 const REDIS_PORT = process.env.PORT || 6379;
 
 const client = redis.createClient(REDIS_PORT);
 
 const app = express();
-app.use(cors());
+app.use(cors())
 
 var map = new Map();
 
@@ -17,16 +18,7 @@ var map = new Map();
 
 // Set Response
 function setResponse(countryName, data) {
-    return `<h1 style="text-align:center">${countryName.toUpperCase()}</h1>
-    <img src=${data.flag} style="display: block;
-    margin-left: auto;
-    margin-right: auto;
-    width: 35%;">
-    <div style="display: flex; text-align: center; margin: auto; justify-content: center;"> 
-        <h2 style="margin-right:20px;">Population: ${data.population}</h2>
-        <h2 style="margin-right:20px;">Capital: ${data.capital}</h2>
-        <h2 style="margin-right:20px;">Weather: ${data.weather}</h2
-    </div>`;
+    return JSON.stringify(data);
 
 //has ${data.population} habitants, the capital is ${data.capital} and is at ${data.weather}º!
 
@@ -56,6 +48,7 @@ async function getPopulation(req, res) {
         client.setex(countryName, 60, JSON.stringify(map.get(countryName)));
 
        // const currentWeather = await getWeather(capital);
+        res.setHeader('Content-Type', 'application/json');
         res.send(setResponse(countryName, map.get(countryName)));
 
     } catch (err) {
@@ -92,7 +85,7 @@ function cache(req, res, next) {
     })
 }
 
-app.get('/country/:countryName', cache, getPopulation);
+app.get('/country/:countryName',cache, getPopulation);
 
 app.listen(5000, () => {
     console.log(`App listening on port ${PORT}`);
